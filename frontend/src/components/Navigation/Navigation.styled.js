fce944bd340e33ae904f740a76d393f1445e8b32
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2022-01-16 15:47:31 +0100

    Fixed Navigation Styled

--- frontend/src/components/Navigation/Navigation.styled.js
+++ frontend/src/components/Navigation/Navigation.styled.js
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 
 export const Nav = styled.div`
+  position: fixed;
+  top: 56px;
+  bottom: 0;
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 20%;
   background-color: #f5f5f5;
+  padding: 10px 20px;
 `;
 
 export const NavElements = styled.h1`
