460724f0fc86fbccc24a35fb56c4a2ac0034acf8
Piotr Rynio <pwrynio@gmail.com>
2022-01-16 02:42:50 +0100

    add example image and fix names of folders

--- frontend/src/view/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div className="App">wITAM</div>;
-}
-
-export default App;
---
+++ frontend/src/views/App.js
@@ -0,0 +1,12 @@
+import exampleImage from 'assets/images/exampleImage.jpeg';
+
+function App() {
+  return (
+    <div>
+      Witam!
+      <img src={exampleImage} alt="example image" />
+    </div>
+  );
+}
+
+export default App;
