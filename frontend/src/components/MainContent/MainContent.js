34da59ef6f598b66907eca2dbbb3e170a52c6dd2
d0mbrow <88898014+BartoszDombrow@users.noreply.github.com>
2022-01-16 17:36:53 +0100

    apperence of the website  (#23)
    * in progres
    
    * Website arrangement
    
    * Update App.js
    
    * Add globalstyles
    
    * Add TopBar and TopBarStyled
    
    * Fixed Navigation Styled
    
    * Fixed RightSide styles
    
    * Finish work on UI/Apperence of the website
    
    * Last change of files name ;)
    
    * Jednak nie
    
    * Last one
    
    * Merge branch 'master' of https://github.com/PiotrRynio/CodersCamp2021.Project-2.React into UI/apperence_of_the_website
    
     Conflicts:
    	frontend/src/index.js
    	frontend/src/view/App.test.js
    	frontend/src/view/viewApp/App.test.js
    	frontend/src/views/App.test.js

---
+++ frontend/src/components/MainContent/MainContent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container } from './MainContent.styled';
+
+const MainContent = () => {
+  return <Container>Main</Container>;
+};
+
+export default MainContent;
