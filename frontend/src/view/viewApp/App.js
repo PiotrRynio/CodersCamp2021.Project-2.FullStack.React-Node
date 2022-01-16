f458c9dbc119ecb7ef7837d7d88170c826de65a3
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2022-01-16 15:11:15 +0100

    Update App.js

--- frontend/src/view/viewApp/App.js
+++ frontend/src/view/viewApp/App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import MainTemplate from '../viewTemplate/MainTemplate';
 import MainContent from '../../components/MainContent/MainContent';
 import RightSide from '../../components/RightSide/RightSide';
+import TopBar from '../../components/Topbar/TopBar';
+import Navigation from '../../components/Navigation/Navigation';
+import { Container } from './App.styled';
 
 function App() {
   return (
-    <>
-      <MainTemplate>
-        <MainContent />
-        <RightSide />
-      </MainTemplate>
-    </>
+    <Container>
+      <TopBar />
+      <Navigation />
+      <MainContent />
+      <RightSide />
+      {/*<MainTemplate>*/}
+      {/*  <MainContent />*/}
+      {/*  <RightSide />*/}
+      {/*</MainTemplate>*/}
+    </Container>
   );
 }
 
---
+++ frontend/src/view/viewApp/App.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: relative;
+  min-height: 100vh;
+  width: 100vw;
+`;
