f9185083218e8bceb055c756e20e8f565df3733c
jaroslawkaczor <10167978+jaroslawkaczor@users.noreply.github.com>
2022-01-18 19:12:54 +0100

    Add CSS styling

--- frontend/src/components/AddPostForm/AddPostForm.js
+++ frontend/src/components/AddPostForm/AddPostForm.js
@@ -2,35 +2,37 @@ import React, { useState } from 'react';
 import { Form, FormTitle, Icon, TitleInput, MessageInput, SubmitInput } from './AddPostForm.styled';
 
 const AddPostForm = () => {
-  const [postObject, setPostObject] = useState({});
+  const [postState, setPostState] = useState({});
 
   const handleChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
-    setPostObject((values) => ({ ...values, [name]: value }));
+    setPostState((prevState) => ({ ...prevState, [name]: value }));
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(postObject);
+    console.log(postState);
   };
 
   return (
     <Form onSubmit={handleSubmit}>
       <FormTitle>Add new post</FormTitle>
-      <TitleInput
-        type="text"
-        name="title"
-        placeholder="Enter title..."
-        value={postObject.title || ''}
-        onChange={handleChange}
-      />
-      <Icon>🌨</Icon>
+      <div>
+        <TitleInput
+          type="text"
+          name="title"
+          placeholder="Enter title..."
+          value={postState.title || ''}
+          onChange={handleChange}
+        />
+        <Icon>🌨</Icon>
+      </div>
       <MessageInput
         type="text"
         name="message"
         placeholder="Enter message..."
-        value={postObject.message || ''}
+        value={postState.message || ''}
         onChange={handleChange}
       />
       <SubmitInput type="submit" value="submit" />
--- frontend/src/components/AddPostForm/AddPostForm.styled.js
+++ frontend/src/components/AddPostForm/AddPostForm.styled.js
@@ -1,23 +1,61 @@
 import styled from 'styled-components';
 
 export const Form = styled.form`
-  padding: 1em;
-
   display: flex;
   flex-direction: column;
   text-align: center;
 
-  background-color: #bbb;
+  padding: 1em;
+  border-radius: 20px;
+  background-color: #bbc;
+
+  * {
+    margin: 3px 0px;
+  }
 `;
 
 export const FormTitle = styled.h2`
+  height: 30px;
+  margin: 10px;
+
   color: gray;
+  font-family: 'Roboto', sans-serif;
+  font-size: 1.5em;
+  font-weight: 300;
 `;
 
-export const Icon = styled.div``;
+export const TitleInput = styled.input`
+  width: 80%;
+  height: 22px;
+  float: left;
 
-export const TitleInput = styled.input``;
+  border-radius: 10px;
+  border: 1px solid #aab;
 
-export const MessageInput = styled.input``;
+  &:hover {
+    border: 1px solid #48f;
+  }
+`;
 
-export const SubmitInput = styled.input``;
+export const Icon = styled.div`
+  #TODO waiting for icon picker
+  
+`;
+
+export const MessageInput = styled.input`
+  height: 6em;
+
+  border-radius: 12px;
+  border: 1px solid #aab;
+
+  &:hover {
+    border: 1px solid #47f;
+  }
+`;
+
+export const SubmitInput = styled.input`
+  border-radius: 10px;
+  border: 1px solid #aab;
+  color: gray;
+  #TODO waiting for the button component
+`;
