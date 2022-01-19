1210f73a9ff26b9a0dc2fc205c24480ba9269d17
Patryk Święickci <patswiecicki@gmail.com>
2022-01-19 17:28:54 +0100

    Fixes after pull

--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Container } from './Home.styled';
 import { CommentList } from '../../components/Comments/UserCommentList/CommentList';
-import Announcement from '../Announcement/Announcement';
 
 const Home = () => {
   return (
