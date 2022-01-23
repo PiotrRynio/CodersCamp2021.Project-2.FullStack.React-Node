2c614282776d6d6bf71a07b50f53fd27b2da6a4a
RadekDulisz <radek@dulisz.com>
2022-01-24 00:37:41 +0100

    quickfix

--- frontend/src/components/DotsButton/DotsButton.styled.jsx
+++ frontend/src/components/DotsButton/DotsButton.styled.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import {breakpoints} from "../../RWD/breakpoints";
+import {breakpoints} from "../../rwd/breakpoints";
 
 export const StyledDotsButton = styled.div`
   position: absolute;
