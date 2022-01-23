355a805b7a0dc8df3dd853a17e99259b76bf8572
RadekDulisz <radek@dulisz.com>
2022-01-23 01:03:03 +0100

    Update frontend/src/components/Dropdown/Dropdown.styled.js
    Co-authored-by: PSwiecicki <patswiecicki@gmail.com>

--- frontend/src/components/Dropdown/Dropdown.styled.js
+++ frontend/src/components/Dropdown/Dropdown.styled.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {breakpoints} from "../../RWD/breakpoints";
+import {breakpoints} from "../../rwd/breakpoints";
 
 export const StyledDropdown = styled.div`
   position: absolute;
