7cada357580da576e2630ec1013202d8208cb076
RadekDulisz <radek@dulisz.com>
2022-01-25 21:32:10 +0100

    fix-prettier & eslint

--- frontend/src/components/Board/BoardTitleSection/DotsButton/DotsButton.styled.jsx
+++ frontend/src/components/Board/BoardTitleSection/DotsButton/DotsButton.styled.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import {breakpoints} from "components/../rwd/breakpoints";
+import { breakpoints } from 'rwd/breakpoints';
 
 export const StyledDotsButton = styled.div`
   position: absolute;
@@ -11,25 +11,25 @@ export const StyledDotsButton = styled.div`
   align-items: center;
   color: #bbb;
   cursor: pointer;
-`
+`;
 
 export const ButtonContainer = styled.div`
   color: #bbb;
   transition: color 0.15s ease-out;
-  
-    &:hover {
-      color: #6b6b6b;
-    }
-  
-    @media ${breakpoints.md} {
-      font-size: 30px;
-    }
-  
-    @media ${breakpoints.lg} {
-      font-size: 40px;
-    }
-  
-    @media ${breakpoints.xl} {
-      font-size: 50px;
-    }
-`
+
+  &:hover {
+    color: #6b6b6b;
+  }
+
+  @media ${breakpoints.md} {
+    font-size: 30px;
+  }
+
+  @media ${breakpoints.lg} {
+    font-size: 40px;
+  }
+
+  @media ${breakpoints.xl} {
+    font-size: 50px;
+  }
+`;
---
+++ frontend/src/components/Board/BoardTitleSection/Dropdown/Dropdown.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyledDropdown, DropdownItem, Image } from './Dropdown.styled';
+import { FaBell, FaFlag, FaEyeSlash } from 'react-icons/fa';
+
+const Dropdown = () => {
+  const dropdownItems = [
+    {
+      icon: <FaBell />,
+      text: 'Notify me',
+    },
+    {
+      icon: <FaFlag />,
+      text: 'Report this board',
+    },
+    {
+      icon: <FaEyeSlash />,
+      text: "Don't show me this board",
+    },
+  ];
+  const displayDropdownItems = dropdownItems.map((dropdownItem) => (
+    <DropdownItem key={dropdownItem.text}>
+      <Image>{dropdownItem.icon}</Image>
+      {dropdownItem.text}
+    </DropdownItem>
+  ));
+
+  return <StyledDropdown>{displayDropdownItems}</StyledDropdown>;
+};
+
+export default Dropdown;
