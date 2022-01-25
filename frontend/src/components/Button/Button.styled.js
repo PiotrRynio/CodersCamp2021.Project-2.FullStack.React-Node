a6df826f7e8bd02495c18343baf3af08bd01f2f4
AgnieszkaPrzybylowska <agnieszka.przybylowska@coderscrew.pl>
2022-01-25 22:45:14 +0100

    Add button component. (#68)
    * Add button and styles. Lack of test.
    
    * Add tests
    
    * style
    
    mi tłumaczono żeby w takiej kolejności robić także tutaj mała poprawka
    
    * Add corrections after CR.
    
    * Last correction.
    
    Co-authored-by: RadekDulisz <radek@dulisz.com>

---
+++ frontend/src/components/Button/Button.styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const StyledButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100px;
+  height: 30px;
+  padding: 15px;
+  color: #ffffff;
+  background-color: #1a8cd8;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  box-shadow: 0px 4px 4px 0px #00000040;
+`;
