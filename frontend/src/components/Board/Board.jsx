da50317f261e401df459b72acaa9ef2bc55a831c
RadekDulisz <radek@dulisz.com>
2022-01-25 18:23:31 +0100

    Board component with styles, no tests yet. (#19)
    * Board component with styles, no tests yet.
    
    * add-styled-component + dropdown-menu
    
    * usuwam niepotrzebne
    
    * add-callback
    
    * add-responsivity
    
    * fixed-styles & proptypes
    
    * add-map
    
    * quickfix
    
    * Delete breakpoints.js
    
    * Update frontend/src/components/Board/Board.styled.js
    
    Co-authored-by: PSwiecicki <patswiecicki@gmail.com>
    
    * Update frontend/src/components/Board/Board.styled.js
    
    Co-authored-by: PSwiecicki <patswiecicki@gmail.com>
    
    * Update frontend/src/components/Dropdown/Dropdown.styled.js
    
    Co-authored-by: PSwiecicki <patswiecicki@gmail.com>
    
    * Update frontend/src/components/Dropdown/Dropdown.js
    
    Co-authored-by: PSwiecicki <patswiecicki@gmail.com>
    
    * quickfix
    
    * more fixes
    
    * even more fixes
    
    * Update frontend/src/components/Board/Board.jsx
    
    Co-authored-by: PSwiecicki <patswiecicki@gmail.com>
    
    * finishing touches
    
    Co-authored-by: PSwiecicki <patswiecicki@gmail.com>

---
+++ frontend/src/components/Board/Board.jsx
@@ -0,0 +1,40 @@
+import {
+  BoardInfoContainer,
+  BoardInfo,
+  BoardImage,
+  BoardTitle,
+  AuthorInfoContainer,
+  AuthorName,
+  AuthorAvatar
+} from "./Board.styled";
+import DotsButton from "../DotsButton/DotsButton";
+import Dropdown from '../Dropdown/Dropdown';
+import { FaUserCircle } from 'react-icons/fa';
+
+const Board = ({user, boardTitle}) => {
+  return (
+      <BoardInfoContainer>
+        <BoardInfo>
+          <BoardImage />
+          {/* TODO: Dodać Link dookoła BoardTitle jak już będzie widok Board'a  */}
+          <BoardTitle>
+            {boardTitle}
+          </BoardTitle>
+          <DotsButton>
+            <Dropdown>
+            </Dropdown>
+          </DotsButton>
+        </BoardInfo>
+        <AuthorInfoContainer>
+          <AuthorName>
+            {user}
+          </AuthorName>
+          <AuthorAvatar >
+            <FaUserCircle />
+          </AuthorAvatar>
+        </AuthorInfoContainer>
+      </BoardInfoContainer>
+  );
+}
+
+export default Board;
---
+++ frontend/src/components/DotsButton/DotsButton.styled.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import {breakpoints} from "components/../rwd/breakpoints";
+
+export const StyledDotsButton = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 60%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #bbb;
+  cursor: pointer;
+`
+
+export const ButtonContainer = styled.div`
+  color: #bbb;
+  transition: color 0.15s ease-out;
+  
+    &:hover {
+      color: #6b6b6b;
+    }
+  
+    @media ${breakpoints.md} {
+      font-size: 30px;
+    }
+  
+    @media ${breakpoints.lg} {
+      font-size: 40px;
+    }
+  
+    @media ${breakpoints.xl} {
+      font-size: 50px;
+    }
+`
