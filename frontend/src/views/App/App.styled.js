a72a52c25bc5dd2099d5e4220e01edf4811ff184
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2022-01-26 15:56:35 +0100

    zamiana width: 100vw; na min-width: 100%; dlatego że po pojawieniu sie kontentu który ma więcej niż height: 100vh; pojawia sie pasek poziomy do przewijania. min-width: 100%; naprawia ten błąd

--- frontend/src/views/App/App.styled.js
+++ frontend/src/views/App/App.styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   position: relative;
   min-height: 100vh;
-  width: 100vw;
+  min-width: 100%;
   display: flex;
   justify-content: center;
 
