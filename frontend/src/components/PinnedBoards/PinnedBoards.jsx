6f49a10d17a0c06117802977b727bcf29a241dac
jaroslawkaczor <10167978+jaroslawkaczor@users.noreply.github.com>
2022-01-27 18:22:31 +0100

    Added board examples

--- frontend/src/components/PinnedBoards/PinnedBoards.jsx
+++ frontend/src/components/PinnedBoards/PinnedBoards.jsx
@@ -1,16 +1,31 @@
-import { Title, TitleMainColor, Icon } from './PinnedBoards.styled';
+import {
+  Container,
+  Title,
+  TitleMainColor,
+  Icon,
+  BoardsEntry,
+  BoardName,
+} from './PinnedBoards.styled';
 import { HiClipboardList } from 'react-icons/hi';
 
 const PinnedBoards = () => {
   return (
-    <>
+    <Container>
       <Icon>
         <HiClipboardList />
       </Icon>
       <Title>
         Pinned<TitleMainColor>Boards</TitleMainColor>
       </Title>
-    </>
+      <BoardsEntry>
+        <HiClipboardList />
+        <BoardName> Board 1 </BoardName>
+      </BoardsEntry>
+      <BoardsEntry>
+        <HiClipboardList />
+        <BoardName> Board 2 </BoardName>
+      </BoardsEntry>
+    </Container>
   );
 };
 
--- frontend/src/components/PinnedBoards/PinnedBoards.styled.js
+++ frontend/src/components/PinnedBoards/PinnedBoards.styled.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+  background-color: gray;
+`;
+
 export const Title = styled.span`
   color: black;
   font-size: 2em;
@@ -14,4 +18,14 @@ export const TitleMainColor = styled.span`
 export const Icon = styled.span`
   font-size: 2em;
   padding: 10px;
+`;
+
+export const BoardsEntry = styled.div`
+  background-color: darkgray;
+`;
+
+export const BoardName = styled.span`
+  color: gray;
+  font-size: 1.5em;
+  font-weight: 500;
 `;
