f5389a11e6e1377ac8777fa9a2bf5eb42c84a639
Patryk Święickci <patswiecicki@gmail.com>
2022-01-30 23:44:24 +0100

    remove component from home.js

--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Container } from './Home.styled';
 import AnnouncementsList from 'components/AnnouncementsList/AnnouncementsList';
-import BoardCreationForm from '../../components/BoardCreationForm/BoardCreationForm';
 
 const Home = () => {
   return (
     <Container>
-      <BoardCreationForm />
       <AnnouncementsList boardsIds={[0, 1]} announcementsNumber={5} showBoardNames={true} />
     </Container>
   );
