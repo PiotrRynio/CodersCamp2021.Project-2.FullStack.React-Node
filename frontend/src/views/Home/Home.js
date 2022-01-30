d9ee3008c7b3fb65bc39d3e16c09cd87462581a7
RadekDulisz <radek@dulisz.com>
2022-01-30 14:51:07 +0100

    view

--- frontend/src/views/Home/Home.js
+++ frontend/src/views/Home/Home.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Container } from './Home.styled';
 import { CommentsSection } from '../../components/CommentsSection/CommentsSection';
 import AnnouncementsList from '../../components/AnnouncementsList/AnnouncementsList';
-
+import Board from '../../components/Board/Board';
 const Home = () => {
   return (
     <Container>
+      <Board />
       <CommentsSection announcementId={1} />
       <AnnouncementsList boardsIds={[1, 2]} announcementsNumber={4} showBoardNames={true} />
     </Container>
