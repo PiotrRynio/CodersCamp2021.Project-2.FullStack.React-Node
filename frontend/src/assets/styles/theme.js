375d9148495c419c354408d9084666ba17cc9d8a
Patryk Święickci <patswiecicki@gmail.com>
2022-01-30 22:45:05 +0100

    Change styles to submit button and add theme

---
+++ frontend/src/assets/styles/theme.js
@@ -0,0 +1,34 @@
+export const theme = {
+  breakpoints: {
+    xxs: '400px',
+    xs: '480px',
+    sm: '560px',
+    md: '768px',
+    lg: '1024px',
+    xl: '1260px',
+    xxl: '1900px',
+  },
+  colors: {
+    primary: '#1a8cd8',
+    secondary: '#fff',
+    primaryLight: '#1fa9ff',
+  },
+  fontFamily: {
+    primary: `Roboto, sans-serif`,
+  },
+  fontSize: {
+    xxs: '8px',
+    xs: '10px',
+    sm: '12px',
+    md: '14px',
+    lg: '20px',
+    xl: '24px',
+    xxl: '32px',
+  },
+  fontWeight: {
+    light: '300',
+    regular: '400',
+    semiBold: '600',
+    bold: '700',
+  },
+};
--- frontend/src/components/BoardCreationForm/BoardCreationForm.styled.js
+++ frontend/src/components/BoardCreationForm/BoardCreationForm.styled.js
@@ -4,30 +4,30 @@ export const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
   padding: 1em;
-  border: 1px solid #1a8cd8;
+  border: 1px solid ${({ theme }) => theme.colors.primary};
   border-radius: 2px;
-  background-color: white;
-  text-align: left;
+  background-color: ${({ theme }) => theme.colors.secondary};
 `;
 
 export const FormTitle = styled.h1`
   width: 250px;
   padding-left: 5px;
-  border-bottom: 2px solid #1a8cd8;
-  font-size: 20px;
+  border-bottom: 2px solid ${({ theme }) => theme.colors.primary};
+  font-size: ${({ theme }) => theme.fontSize.lg};
 `;
 
 export const MarkedTitle = styled.span`
-  color: #1a8cd8;
+  color: ${({ theme }) => theme.colors.primary};
 `;
 
 export const BoardTitleInput = styled.input`
   padding: 5px;
-  border: 2px solid #1a8cd8;
+  border: 2px solid ${({ theme }) => theme.colors.primary};
   border-radius: 5px;
+  font-family: ${({ theme }) => theme.fontFamily.primary};
 
   &:focus {
-    outline: 1px solid #1a8cd8;
+    outline: 1px solid ${({ theme }) => theme.colors.primary};
   }
 `;
 
@@ -43,14 +43,14 @@ export const HiddenInput = styled.input`
 
 export const StyledIconPicker = styled.div`
   padding: 5px;
-  border: 2px solid #1a8cd8;
+  border: 2px solid ${({ theme }) => theme.colors.primary};
   border-radius: 5px;
   color: #555;
   font-size: 13px;
 
   &:hover {
-    background-color: #1a8cd8;
-    color: white;
+    background-color: ${({ theme }) => theme.colors.primary};
+    color: ${({ theme }) => theme.colors.secondary};
   }
 `;
 
@@ -58,35 +58,37 @@ export const ContentInput = styled.textarea`
   width: 100%;
   height: 100px;
   padding: 5px;
-  border: 2px solid #1a8cd8;
+  border: 2px solid ${({ theme }) => theme.colors.primary};
   border-radius: 4px;
   text-align: justify;
+  font-family: ${({ theme }) => theme.fontFamily.primary};
   resize: none;
 
   &:focus {
-    outline: 1px solid #1a8cd8;
+    outline: 1px solid ${({ theme }) => theme.colors.primary};
   }
 `;
 
 export const StyledSelect = styled.select`
   padding: 5px;
-  border: 2px solid #1a8cd8;
+  border: 2px solid ${({ theme }) => theme.colors.primary};
   border-radius: 4px;
 
   &:focus {
-    outline: 1px solid #1a8cd8;
+    outline: 1px solid ${({ theme }) => theme.colors.primary};
   }
 `;
 
 export const StyledButton = styled.button`
   margin-top: 10px;
   padding: 5px;
-  border: 2px solid #1a8cd8;
+  border: 2px solid ${({ theme }) => theme.colors.primary};
   border-radius: 4px;
-  background-color: white;
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.secondary};
 
   &:hover {
-    background-color: #1a8cd8;
-    color: white;
+    background-color: ${({ theme }) => theme.colors.primaryLight};
+    border: 2px solid ${({ theme }) => theme.colors.primaryLight};
   }
 `;
