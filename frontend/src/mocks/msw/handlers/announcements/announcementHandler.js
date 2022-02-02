80225997a21fbb0b7eb27b191f4d19b8fe2ce4f2
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2022-02-02 19:02:22 +0100

    add board preview

--- frontend/src/mocks/msw/handlers/announcements/announcementHandler.js
+++ frontend/src/mocks/msw/handlers/announcements/announcementHandler.js
@@ -3,7 +3,6 @@ import { announcementsResponse } from './announcementResponse';
 
 const announcementsHandler = rest.get('/announcements/:id', (req, res, ctx) => {
   const { id } = req.params;
-  console.log(id);
   return res(ctx.status(200), ctx.json(announcementsResponse.announcements[id]));
 });
 
