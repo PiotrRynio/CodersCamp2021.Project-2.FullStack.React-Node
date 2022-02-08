8e353900d1a930e661d396bae38c8179fa683235
PSwiecicki <patswiecicki@gmail.com>
2022-02-08 23:11:29 +0100

    Boards list (#91)
    * Fix board
    
    * add navigation function
    
    * add distance
    
    * add additional response
    
    * add additional response
    
    * remove old console.log
    
    * clear after CR

--- frontend/src/mocks/msw/rest-api/boards/responses/boardsResponse.js
+++ frontend/src/mocks/msw/rest-api/boards/responses/boardsResponse.js
@@ -1,5 +1,6 @@
 import avatar1 from 'mocks/images/avatars/sample-avatar1.jpg';
 import avatar2 from 'mocks/images/avatars/sample-avatar2.jpg';
+import avatar3 from 'mocks/images/avatars/sample-avatar3.jpg';
 import { announcementsResponse } from 'mocks/msw/rest-api/announcements/responses/announcementResponse';
 
 const [
@@ -16,6 +17,7 @@ const sampleBoard1 = {
   announcements: [sampleAnnouncement1, sampleAnnouncement2, sampleAnnouncement3],
   avatar: avatar1,
   author: 'Artur',
+  cords: { latitude: 50.90005960872424, longitude: 16.744514564579045 },
   description:
     'Sobótka Description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet dictum sit amet justo donec enim diam vulputate. Enim sed faucibus turpis in eu mi. Nibh cras pulvinar mattis nunc sed blandit libero. Cras tincidunt lobortis feugiat vivamus at augue eget arcu.',
 };
@@ -25,15 +27,20 @@ const sampleBoard2 = {
   announcements: [sampleAnnouncement4, sampleAnnouncement5],
   author: 'Krzysztof',
   avatar: avatar2,
+  cords: { latitude: 54.343777269198945, longitude: 18.662549271679243 },
   description:
     'To drugie description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet dictum sit amet justo donec enim diam vulputate. Enim sed faucibus turpis in eu mi. Nibh cras pulvinar mattis nunc sed blandit libero. Cras tincidunt lobortis feugiat vivamus at augue eget arcu.',
 };
 
 const sampleBoard3 = {
   id: 2,
-  boardName: 'Opole',
+  boardName: 'Knurów',
   announcements: [sampleAnnouncement4, sampleAnnouncement5],
-  author: 'Krzysztof',
+  author: 'Świnek',
+  avatar: avatar3,
+  cords: { latitude: 53.90178364190212, longitude: 14.237340052681517 },
+  description:
+    'Świniów. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet dictum sit amet justo donec enim diam vulputate. Enim sed faucibus turpis in eu mi. Nibh cras pulvinar mattis nunc sed blandit libero. Cras tincidunt lobortis feugiat vivamus at augue eget arcu.',
 };
 const sampleBoard4 = {
   id: 3,
@@ -77,4 +84,4 @@ export const availableForUserBoardsResponse = {
   boards: [sampleBoard4, sampleBoard5, sampleBoard6, sampleBoard7],
 };
 export const UserAlreadyAssignedBoardsResponse = { boards: [sampleBoard4, sampleBoard5] };
-export const boardsResponse = { boards: [sampleBoard1, sampleBoard2] };
+export const boardsResponse = { boards: [sampleBoard1, sampleBoard2, sampleBoard3] };
