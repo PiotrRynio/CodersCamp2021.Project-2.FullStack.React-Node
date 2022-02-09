459753b31ff15e29f434ae523019f9f44ab60d87
AgnieszkaPrzybylowska <agnieszka.przybylowska@coderscrew.pl>
2022-02-09 16:27:01 +0100

    Add tests. Needed test to check if components show properly data after receive.

--- frontend/src/components/SubscribedBoardsList/SubscribedBoardsList.test.js
+++ frontend/src/components/SubscribedBoardsList/SubscribedBoardsList.test.js
@@ -1,16 +1,59 @@
 import { render, screen } from '../../test-utils';
 import { SubscribedBoardsList } from './SubscribedBoardsList';
+import {
+  getBoardsWillReturn,
+  getBoardsWillReturnFail,
+} from '../../mocks/msw/rest-api/boards/mockEndpoints/boardsAnnouncementsMockEndpoints';
+import { waitFor, waitForElementToBeRemoved } from '@testing-library/react';
+import avatar1 from '../../mocks/images/avatars/sample-avatar1.jpg';
 
 describe('SubscribedBoardsList', () => {
+  const fakeBoard1 = {
+    id: 1,
+    icon: avatar1,
+    boardName: 'TestBoardName1',
+    announcements: [],
+    author: 'TestAuthor1',
+  };
+
+  const fakeBoard2 = {
+    id: 2,
+    icon: avatar1,
+    boardName: 'TestBoardName2',
+    announcements: [],
+    author: 'TestAuthor2',
+  };
   it('Should properly render component', () => {
     //when
     render(<SubscribedBoardsList />);
   });
-  it('Should show status when component is loading', () => {
+  it('Should display status when component is loading', () => {
     //when
     render(<SubscribedBoardsList />);
     // then
     const loadingStatus = screen.getByText(/Loading/i);
     expect(loadingStatus).toBeInTheDocument();
   });
+  it('Should display status when error occurred', async () => {
+    // given
+    getBoardsWillReturnFail();
+    render(<SubscribedBoardsList />);
+
+    // when
+    const errorStatus = await waitFor(() => screen.findByText(/Error/i), { timeout: 10000 });
+    // then
+    expect(errorStatus).toBeInTheDocument();
+  });
+  /*  it('Should properly display subscribed boards list', async () => {
+    const fakeBoardsResponse = { boards: [fakeBoard1, fakeBoard2] };
+
+    // given
+    getBoardsWillReturn(fakeBoardsResponse);
+    render(<SubscribedBoardsList />);
+
+    //when
+    const boardName = await waitFor(() => screen.findByText(/TestBoardName1/i), { timeout: 10000 });
+    // then
+    //screen.getByText('TestBoardName1');
+  });*/
 });
--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom';
 import { server } from 'mocks/msw/rest-api/server';
+jest.setTimeout(10000);
 
 beforeAll(() => {
   server.listen();
