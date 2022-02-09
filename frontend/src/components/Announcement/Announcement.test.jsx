c81fd5859c5f611b8a6ed14310263f61118618dd
Patryk Święickci <patswiecicki@gmail.com>
2022-02-09 23:13:56 +0100

    Fix after CR

--- frontend/src/components/Announcement/Announcement.test.jsx
+++ frontend/src/components/Announcement/Announcement.test.jsx
@@ -10,40 +10,36 @@ describe('Announcement', () => {
     content:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris in risus eros. Morbi euismod magna ut lacus luctus tempus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce leo eros, pretium et blandit vestibulum, accumsan nec tellus. Pellentesque odio est, posuere at lectus ac, fermentum dignissim purus. Pellentesque convallis faucibus ex sed sollicitudin. Vestibulum sodales quis felis eget facilisis. Integer venenatis, tellus in consectetur eleifend, risus nulla feugiat nisl, a ornare diam urna sed risus. Maecenas a volutpat est. Maecenas mattis odio a ligula euismod, sit amet viverra massa aliquet.\n',
     comments: ['a', 'b', 'c'],
-    date: new Date(1995, 11, 17, 3, 24, 0),
+    date: new Date(),
   };
 
   it('Should properly render announcement', async () => {
-    //given
-
     //when
     render(
       <Router>
         <Announcement announcement={fakeAnnouncement} />
       </Router>,
     );
+
+    // then
     const title = screen.getByText(fakeAnnouncement.title);
     const boardName = screen.queryByText(fakeAnnouncement.boardName);
     const commentsText = screen.getByText(`[${fakeAnnouncement.comments.length} comments]`);
-
-    // then
     expect(title).toBeInTheDocument();
     expect(boardName).not.toBeInTheDocument();
     expect(commentsText).toBeInTheDocument();
   });
 
   it('Should properly render announcement with board name', async () => {
-    //given
-
     //when
     render(
       <Router>
         <Announcement announcement={fakeAnnouncement} showBoard={true} />
       </Router>,
     );
-    const boardName = screen.queryByText(fakeAnnouncement.boardName);
 
     // then
+    const boardName = screen.queryByText(fakeAnnouncement.boardName);
     expect(boardName).toBeInTheDocument();
   });
 
@@ -56,10 +52,10 @@ describe('Announcement', () => {
         <Announcement announcement={fakeAnnouncement} isOpen={true} />
       </Router>,
     );
-    const commentsText = screen.queryByText(`[${fakeAnnouncement.comments.length} comments]`);
-    const content = screen.getByText(/Vestibulum sodales quis felis eget facilisis/i);
 
     // then
+    const commentsText = screen.queryByText(`[${fakeAnnouncement.comments.length} comments]`);
+    const content = screen.getByText(/Vestibulum sodales quis felis eget facilisis/i);
     expect(content).toBeInTheDocument();
     expect(commentsText).not.toBeInTheDocument();
   });
