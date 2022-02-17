ec219d5e49a58091e3818b40115ab5492ee147eb
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2022-02-18 00:21:56 +0100

    add morgan

--- backend/src/App.js
+++ backend/src/App.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 import { helloWorldController } from './controller/HelloWorld/HelloWorld.controller.js';
 
 dotenv.config();
@@ -9,6 +10,8 @@ export const app = async () => {
   const restApiServer = express();
   restApiServer.use(cors());
   restApiServer.use(express.json());
+  restApiServer.use(express.urlencoded({ extended: true }));
+  restApiServer.use(morgan('combined'));
 
   restApiServer.use('/rest-api', helloWorldController());
 
