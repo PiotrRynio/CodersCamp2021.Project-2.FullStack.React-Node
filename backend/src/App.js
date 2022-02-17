53cfabdcdd97de9fcc546144d5fa20ca57f6287e
Piotr Rynio <pwrynio@gmail.com>
2022-02-17 23:19:35 +0100

    feat(helloWorld): add next part

--- backend/src/App.js
+++ backend/src/App.js
@@ -2,11 +2,15 @@ import express from 'express';
 // import cors from 'cors';
 // import { HelloWorldController } from './controller/HelloWorld/HelloWorld.controller.js';
 import dotenv from 'dotenv';
+import {HelloWorldController} from "./controller/HelloWorld/HelloWorld.controller.js";
 
 dotenv.config();
 
 export const app = async () => {
   const restApiServer = express();
+
+ new HelloWorldController(restApiServer);
+
 
   return restApiServer;
 };
