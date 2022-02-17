dcd0b6232c622cdba9c8e846f9f194e3774ef471
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2022-02-17 23:53:25 +0100

    config eslint

--- backend/src/App.js
+++ backend/src/App.js
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 import { HelloWorldController } from './controller/HelloWorld/HelloWorld.controller.js';
 
 dotenv.config();
-
 export const app = async () => {
   const restApiServer = express();
-  app.use(cors());
+  restApiServer.use(cors());
+
   new HelloWorldController(restApiServer);
 
   return restApiServer;
