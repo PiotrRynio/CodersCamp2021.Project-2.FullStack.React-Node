08db0dc01bc6de714e2cf2acf712862abfc25a96
d0mbrow <88898014+BartoszDombrow@users.noreply.github.com>
2022-02-17 22:35:37 +0100

    config eslint (#123)

--- backend/src/App.js
+++ backend/src/App.js
@@ -1,10 +1,9 @@
 import express from 'express';
-// eslint-disable-next-line import/no-extraneous-dependencies
+
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-// eslint-disable-next-line import/prefer-default-export
 export const app = async () => {
   const restApiServer = express();
 
