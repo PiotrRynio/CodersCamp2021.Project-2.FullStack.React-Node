c0471899a4af1665af3f9afcbd5346d711166c3e
AgnieszkaPrzybylowska <agnieszka.przybylowska@coderscrew.pl>
2022-02-18 01:42:40 +0100

    Feat/hello world rest api call  (#122)
    * feat(server): add initial server.
    
    * feat(server): Add server
    
    * feat(hello world): add controller, repository and service
    
    * feat(package): add install script
    
    * feat(gitignore): add backend modules
    
    * feat(helloWorld): add next part
    
    * config eslint
    
    * config eslint
    
    * feat(helloWorld): update controller
    
    * feat(helloWorld): update controller
    
    * add morgan
    
    * feat(helloWorld): update
    
    * feat(helloWorld): add comment
    
    * add conf
    
    * Add validation
    
    * fix(marge): Fix after marge
    
    * add connection to database mango
    
    * add connection to database mango
    
    Co-authored-by: Piotr Rynio <pwrynio@gmail.com>
    Co-authored-by: Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
    Co-authored-by: Patryk Święickci <patswiecicki@gmail.com>

---
+++ backend/src/repository/HelloWorld/HelloWorld.repository.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+import { HelloWorld } from '../../service/HelloWorld/HelloWorld.js';
+const { Schema } = mongoose;
+
+export class HelloWorldRepository {
+  // eslint-disable-next-line class-methods-use-this
+  static async findAll() {
+    console.log('jestesmy w find all ');
+    // return 'HELLO WORLD';
+
+    // eslint-disable-next-line no-use-before-define
+    const mongoFindResult = await MongoHelloWorld.find();
+    // eslint-disable-next-line no-use-before-define
+    console.log('');
+    console.log('jestesmy na koncu ');
+    return mongoFindResult.map((mongoDocument) => mongoDocumentToDomain(mongoDocument));
+  }
+}
+
+const HelloWorldSchema = new mongoose.Schema({
+  _id: Schema.Types.String,
+  helloWorldText: Schema.Types.String,
+});
+
+const MongoHelloWorld = mongoose.model('HelloWorldSchema', HelloWorldSchema);
+
+function mongoDocumentToDomain(mongoDocument) {
+  return new HelloWorld({
+    // eslint-disable-next-line no-underscore-dangle
+    helloWorldId: mongoDocument._id,
+    helloWorldText: mongoDocument.helloWorldText,
+  });
+}
