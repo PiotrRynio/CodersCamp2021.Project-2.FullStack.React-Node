df287a84151edf649c70607c9ecb1c01c4085110
Bartosz Dombrow <bartosz.dombrow1911@gmail.com>
2022-02-19 15:58:41 +0100

    add connection to database mango

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
+import express from 'express';
+import cors from 'cors';
 import { app } from './src/App.js';
-
+import routesUrls from './src/routes/routes.js';
 console.log('Application was started');
 
 const port = process.env.REST_API_PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+app.use('/', routesUrls);
 
 app().then((restApiServer) => {
   restApiServer.listen(port);
