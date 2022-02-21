d047dffa884cdeb869143c35fa970da506523862
Piotr Rynio <pwrynio@gmail.com>
2022-02-21 21:40:18 +0100

    feat(helloWorld): learning data

--- backend/src/controller/HelloWorld/HelloWorld.controller.js
+++ backend/src/controller/HelloWorld/HelloWorld.controller.js
@@ -6,13 +6,10 @@ export function helloWorldController() {
 
   router
     .route('/hello-world')
-    // .get(HelloWorldService.getHelloWorld)
     // .get(() => HelloWorldService.getHelloWorld())
     .post((request, response) => {
-      console.log('0000000000000');
-
       HelloWorldService.postHelloWorld('arg')
-        .then((data)=>data)
+        .then((data) => data)
         .catch((error) => {
           response.status(400).send({ message: error.message });
         });
@@ -20,19 +17,3 @@ export function helloWorldController() {
 
   return router;
 }
-
-// router
-//   .route('/hello-world')
-//   .get((request, response) => () => {
-//     console.log('uuu');
-//     HelloWorldService.getHelloWorld()
-//       .then((data) => {
-//         console.log('aaa');
-//         response.status(200).json(data);
-//       })
-//       .catch((error) => {
-//         console.log('elo');
-//         console.log(error.message);
-//         response.status(400).json(error.message);
-//       });
-//   })
--- backend/src/service/HelloWorld/HelloWorld.service.js
+++ backend/src/service/HelloWorld/HelloWorld.service.js
@@ -6,24 +6,11 @@ export class HelloWorldService {
     await HelloWorldRepository.findAll({}, (err, doc) => doc).then((doc) =>
       res.status(200).json(doc),
     );
-    // TODO: Czy to nie będzie dla Nas zawsze true?
-    // TODO: .catch((err) => res.status(500).json({ message: `Server error: ${err}` }));
   }
 
   static async postHelloWorld(helloWorldName) {
-    console.log('1111111111');
-    // const { error } = helloWorldValidationSchema.validate(helloWorldName };
     throw new Error('nasz error');
 
-    // // console.log(error);
-    // if (error) {
-    //   // return res.status(400).send(error.details[0].message);
-    //   //
-    //   //
-    //   // return { type: 'error', message: error.details[0].message };
-    // }
-
-    console.log('222222');
     const hello = { name: req.body.name };
 
     res.status(200).json(hello);
