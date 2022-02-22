bad0e5b600569133dacdab831830f971b19dab1b
Piotr Rynio <41823689+PiotrRynio@users.noreply.github.com>
2022-02-22 22:09:53 +0100

    feat(helloWorld): remove hello world and change location of common things (#149)

--- backend/src/App.js
+++ backend/src/App.js
@@ -2,8 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
-import { helloWorldController } from './controller/HelloWorld/HelloWorld.controller.js';
-import { connectToMongoDb } from './utils/dataBase/mongo/mongoDB.js';
+import { connectToMongoDb } from './common/repository/mongo/mongoDB.js';
 dotenv.config();
 
 export const app = async () => {
@@ -14,8 +13,6 @@ export const app = async () => {
   restApiServer.use(express.json());
   restApiServer.use(express.urlencoded({ extended: true }));
   restApiServer.use(morgan('combined'));
-
-  restApiServer.use('/rest-api', helloWorldController());
 
   return restApiServer;
 };
