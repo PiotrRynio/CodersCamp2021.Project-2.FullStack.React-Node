13d1f802edf6692d51386196b7c647c5238abafb
Piotr Rynio <pwrynio@gmail.com>
2022-02-23 18:44:04 +0100

    feat(userRegistration): update App.js

--- backend/src/App.js
+++ backend/src/App.js
@@ -3,19 +3,29 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import { connectToMongoDb } from './common/repository/mongo/mongoDB.js';
-import { userRegistrationDetailsController } from './controller/UserRegistrationDetails/UserRegistrationDetails.controller.js';
+import { UserRegistrationDetailsController } from './controller/UserRegistrationDetails/UserRegistrationDetails.controller.js';
+import { UserRegistrationDetailRepository } from './repository/UserRegistrationDetails/UserRegistrationDetail.repository.js';
+import { UserRegistrationDetailsService } from './service/UserRegistrationDetails/UserRegistrationDetails.service.js';
 
 dotenv.config();
 
 export const app = async () => {
   await connectToMongoDb();
 
+  const userRegistrationDetailRepository = new UserRegistrationDetailRepository();
+  const userRegistrationDetailsService = new UserRegistrationDetailsService(
+    userRegistrationDetailRepository,
+  );
+  const userRegistrationDetailsController = new UserRegistrationDetailsController(
+    userRegistrationDetailsService,
+  );
+
   const restApiServer = express();
   restApiServer.use(cors());
   restApiServer.use(express.json());
   restApiServer.use(express.urlencoded({ extended: true }));
   restApiServer.use(morgan('combined'));
-  restApiServer.use('/rest-api', userRegistrationDetailsController());
+  restApiServer.use('/rest-api', userRegistrationDetailsController);
 
   return restApiServer;
 };
