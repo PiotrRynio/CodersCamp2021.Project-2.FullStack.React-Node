100b7385b5cababa58e730a2f9310e49b4f562f3
jaroslawkaczor <10167978+jaroslawkaczor@users.noreply.github.com>
2022-02-24 18:13:03 +0100

    Refactor

--- backend/src/modules/AddComment/service/AddComment.js
+++ backend/src/modules/AddComment/service/AddComment.js
@@ -1,8 +1,6 @@
-import { v4 as uuidv4 } from 'uuid';
-
 export class AddComment {
   constructor(props) {
-    this.userId = props.userId || uuidv4();
+    this.userId = props.userId;
     this.board = props.board;
     this.content = props.content;
   }
