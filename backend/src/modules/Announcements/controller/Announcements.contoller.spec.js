418e08b00cc82d9c7e87b4003bda2cb43e1d2f9b
Patryk Święickci <patswiecicki@gmail.com>
2022-02-26 13:35:15 +0100

    test(Announcement): Add announcement controller tests

---
+++ backend/src/modules/Announcements/controller/Announcements.contoller.spec.js
@@ -0,0 +1,63 @@
+import { agent } from 'supertest';
+import { AnnouncementsController } from './Announcements.controller.js';
+import { testApi } from '../../../testsUtils/testApi.js';
+
+describe('AnnouncementsController |', () => {
+  const postAnnouncementRequestBody = {
+    title: 'Brak wody',
+    boardName: 'Sobótka',
+    content: 'Nie będzie wody tego i tego dnia, tra la la.',
+    date: Date.now(),
+  };
+
+  test('POST /rest-api/borders/:id/announcements | when return boardsAnnouncements with new Announcements', async () => {
+    // GIVEN
+    const expectedResponse = {
+      announcements: [
+        {
+          ...postAnnouncementRequestBody,
+          id: 'newId',
+          commentsIds: [],
+        },
+      ],
+    };
+    const testService = {
+      addAnnouncement: async (postAnnouncementRequestBody) => {
+        postAnnouncementRequestBody.id = 'newId';
+        postAnnouncementRequestBody.commentsIds = [];
+        return [postAnnouncementRequestBody];
+      },
+    };
+    const announcementsController = new AnnouncementsController(testService);
+    const app = testApi('/rest-api', announcementsController.router);
+
+    // WHEN
+    const { body, status } = await agent(app, {})
+      .post('/rest-api/boards/1/announcements')
+      .send(postAnnouncementRequestBody);
+
+    // THEN
+    expect(status).toEqual(200);
+    expect(body).toEqual(expectedResponse);
+  });
+
+  test('POST /rest-api/borders/:id/announcements | when service throw Error', async () => {
+    // GIVEN
+    const testService = {
+      addAnnouncement: async () => {
+        throw new Error('Error message');
+      },
+    };
+    const announcementsController = new AnnouncementsController(testService);
+    const app = testApi('/rest-api', announcementsController.router);
+
+    // WHEN
+    const { body, status } = await agent(app, {})
+      .post('/rest-api/boards/1/announcements')
+      .send(postAnnouncementRequestBody);
+
+    // THEN
+    expect(status).toEqual(400);
+    expect(body).toEqual({ message: 'Error message' });
+  });
+});
