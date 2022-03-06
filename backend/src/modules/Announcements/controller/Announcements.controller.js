518b26137f752daea6c7a0f80d3a91fc32d55de9
Patryk Święcicki <patswiecicki@gmail.com>
2022-03-06 19:32:43 +0100

    feat(Announcement): Add controller for get boards announcements

--- backend/src/modules/Announcements/controller/Announcements.controller.js
+++ backend/src/modules/Announcements/controller/Announcements.controller.js
@@ -1,23 +1,36 @@
 import { Router } from 'express';
-import Announcement from '../service/Announcement.js';
 
 export class AnnouncementsController {
   constructor(service) {
     this.service = service;
     this.router = Router();
 
-    this.router.route('/boards/:id/announcements').post((request, response) => {
-      const id = request.params.id;
-      this.service
-        .addAnnouncement(request.body, id)
-        .then((announcements) => {
-          const returnedData = { announcements: announcements };
-          response.status(200).send(returnedData);
-        })
-        .catch((error) => {
-          response.status(400).send({ message: error.message });
-        });
-    });
+    this.router
+      .route('/boards/:id/announcements')
+      .post((request, response) => {
+        const id = request.params.id;
+        this.service
+          .addAnnouncement(request.body, id)
+          .then((announcements) => {
+            const returnedData = { announcements: announcements };
+            response.status(200).send(returnedData);
+          })
+          .catch((error) => {
+            response.status(400).send({ message: error.message });
+          });
+      })
+      .get((request, response) => {
+        const id = request.params.id;
+        this.service
+          .findBoardAnnouncements(id)
+          .then((announcements) => {
+            const returnedData = { announcements: announcements };
+            response.status(200).send(returnedData);
+          })
+          .catch((error) => {
+            response.status(400).send({ message: error.message });
+          });
+      });
 
     this.router.route('/announcements/:id').get((request, response) => {
       const id = request.params.id;
