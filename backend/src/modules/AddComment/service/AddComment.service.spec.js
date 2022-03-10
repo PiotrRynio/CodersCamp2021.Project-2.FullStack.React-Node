939b1653414b57b63e1225f0ad062ebbca277b6c
RadekDulisz <radek@dulisz.com>
2022-03-10 21:56:50 +0100

    fix-add-comment

--- backend/src/modules/AddComment/service/AddComment.service.spec.js
+++ backend/src/modules/AddComment/service/AddComment.service.spec.js
@@ -1,10 +1,6 @@
 import { InMemoryCommentRepository } from '../repository/inMemory/inMemoryAddComment.repository.js';
 import { AddCommentService } from './AddComment.service.js';
 import { Comment } from './AddComment.js';
-import {
-  InMemoryAnnouncementRepository
-} from "../../Announcements/repository/inMemory/InMemoryAnnouncement.repository.js";
-import {AnnouncementsService} from "../../Announcements/service/Announcements.service.js";
 
 describe('AddCommentRepository|', () => {
   const defaultComment = new Comment({
@@ -12,6 +8,11 @@ describe('AddCommentRepository|', () => {
     content: 'tresc komentarza',
     date: 'dzis',
   });
+
+  const commentWithoutContent = {
+    userId: '8a4a5d2d-af04-4f3a-87d1-ef8130f8bbb1',
+    date: 'dzis',
+  }
 
   test('When the correct comment is added return content of the comment', async () => {
     const inMemoryCommentRepository = new InMemoryCommentRepository();
@@ -21,22 +22,14 @@ describe('AddCommentRepository|', () => {
 
     expect(returnedContent).toBe(defaultComment.content);
   });
-  test('when announcement data is not valid, then error is throw', async () => {
-    //Given
-    const inMemoryAnnouncementRepository = new InMemoryAnnouncementRepository();
-    const announcementService = new AnnouncementsService(inMemoryAnnouncementRepository);
+  test('when comment data is not valid then throw eror', async () => {
+    const inMemoryCommentRepository = new InMemoryCommentRepository();
+    const addCommentService = new AddCommentService(inMemoryCommentRepository);
 
-    //When
-    const addAnnouncementWithoutContent = async () => {
-      await announcementService.addAnnouncement(announcementDataWithoutContent, boardId);
+    const addCommentWithoutContent = async () => {
+      await addCommentService.addComment(commentWithoutContent);
     };
 
-    const addAnnouncementWithToShortTitle = async () => {
-      await announcementService.addAnnouncement(announcementDataWithToShortTitle, boardId);
-    };
-
-    //Then
-    await expect(addAnnouncementWithoutContent).rejects.toThrowError();
-    await expect(addAnnouncementWithToShortTitle).rejects.toThrowError();
+    await expect(addCommentWithoutContent).rejects.toThrowError();
   });
 });
