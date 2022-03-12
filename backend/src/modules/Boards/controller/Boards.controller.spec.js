94d3bffc6eda7a112c094338af34a3266bf3beca
AgnieszkaPrzybylowska <agnieszka.przybylowska@coderscrew.pl>
2022-03-12 17:31:00 +0100

    test: add controller tests.

--- backend/src/modules/Boards/controller/Boards.controller.spec.js
+++ backend/src/modules/Boards/controller/Boards.controller.spec.js
@@ -3,16 +3,17 @@ import { testApi } from '../../../testUtils/testApi.js';
 import { BoardsController } from './BoardsController.js';
 
 describe('Boards controller |', () => {
-  const boardBody = {
+  const testBoardBody = {
     boardName: 'Wroclaw',
     coords: { latitude: 51.88569995139321, longitude: 17.02390643626451 },
     accessType: 'public',
     adminId: '507f191e810c19729de860ea',
     description: 'sample description',
     avatarUrl: 'https://firebasestorage.googleapis.com/sampleavatarurl',
+    announcements: [],
   };
 
-  test('POST /rest-api/boards | when return added body', async () => {
+  test('POST /rest-api/boards | when return added new board body', async () => {
     // GIVEN
     const testService = {
       addBoard: async (boardBody) => boardBody,
@@ -21,14 +22,73 @@ describe('Boards controller |', () => {
     const app = testApi('/rest-api', boardsController.router);
 
     // WHEN
-    const { body, status } = await agent(app).post('/rest-api/boards').send(boardBody);
+    const { body, status } = await agent(app).post('/rest-api/boards').send(testBoardBody);
 
     // THEN
     expect(status).toEqual(200);
-    expect(body.returnedData.boardName).toEqual(boardBody.boardName);
-    expect(body.returnedData.mapCoordinates).toEqual(boardBody.mapCoordinates);
-    expect(body.returnedData.accessType).toEqual(boardBody.accessType);
-    expect(body.returnedData.adminId).toEqual(boardBody.adminId);
-    expect(body.returnedData.description).toEqual(boardBody.description);
+    expect(body.returnedData.boardName).toEqual(testBoardBody.boardName);
+    expect(body.returnedData.mapCoordinates).toEqual(testBoardBody.mapCoordinates);
+    expect(body.returnedData.accessType).toEqual(testBoardBody.accessType);
+    expect(body.returnedData.adminId).toEqual(testBoardBody.adminId);
+    expect(body.returnedData.description).toEqual(testBoardBody.description);
+  });
+
+  test('POST /rest-api/boards/ | when service thrown an error', async () => {
+    // GIVEN
+    const testService = {
+      addBoard: async () => {
+        throw new Error('Sample error message');
+      },
+    };
+    const boardsController = new BoardsController(testService);
+    const app = testApi('/rest-api', boardsController.router);
+
+    // WHEN
+    const { body, status } = await agent(app).post('/rest-api/boards').send(testBoardBody);
+
+    // THEN
+    expect(status).toEqual(400);
+    expect(body).toEqual({ message: 'Sample error message' });
+  });
+  test('PATCH /rest-api/boards/:id | when return board with added announcement', async () => {
+    // GIVEN
+    const testAnnouncementID = '622a38a4e30a4ba39ee75a40';
+    const testBoardID = '622a38b8e7cfef8c44142cd9';
+    testBoardBody.id = testBoardID;
+    testBoardBody.announcements.push(testAnnouncementID);
+    const testService = {
+      addNewAnnouncement: async (testBoardID, testAnnouncementID) => testBoardBody,
+    };
+    const boardsController = new BoardsController(testService);
+    const app = testApi('/rest-api', boardsController.router);
+
+    // WHEN
+    const { body, status } = await agent(app).patch(`/rest-api/boards/${testBoardID}`);
+
+    // THEN
+    expect(status).toEqual(200);
+    expect(body.returnedData).toEqual(testBoardBody);
+  });
+
+  test('PATCH /rest-api/boards/:id | when service thrown an error', async () => {
+    // GIVEN
+    const testAnnouncementID = '622a38a4e30a4ba39ee75a40';
+    const testBoardID = '622a38b8e7cfef8c44142cd9';
+    testBoardBody.id = testBoardID;
+    testBoardBody.announcements.push(testAnnouncementID);
+    const testService = {
+      addNewAnnouncement: async () => {
+        throw new Error('Sample error message');
+      },
+    };
+    const boardsController = new BoardsController(testService);
+    const app = testApi('/rest-api', boardsController.router);
+
+    // WHEN
+    const { body, status } = await agent(app).patch(`/rest-api/boards/${testBoardID}`);
+
+    // THEN
+    expect(status).toEqual(400);
+    expect(body).toEqual({ message: 'Sample error message' });
   });
 });
