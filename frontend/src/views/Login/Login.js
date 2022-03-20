7ee12f5db3a2e4d53d668a283b09b916f9f771f5
AgnieszkaPrzybylowska <agnieszka.przybylowska@coderscrew.pl>
2022-03-20 22:16:58 +0100

    fix: refactor and repair in memory repository createNewUser method.

--- frontend/src/views/Login/Login.js
+++ frontend/src/views/Login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Container,
   LogoSection,
@@ -20,21 +20,39 @@ import {
 import { useContext } from 'react';
 import { useLocation, useNavigate } from 'react-router';
 import { UserContext } from 'providers/AppProviders';
+import { useMutation } from 'react-query';
+import { REST_API_URL } from '../../constants/restApiPaths';
 
 const LogIn = () => {
   const { user, setUser } = useContext(UserContext);
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+
   const navigate = useNavigate();
   const location = useLocation();
 
+  const { mutate } = useMutation((loginData) => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(loginData),
+    };
+    const loginUrl = `${REST_API_URL}/log-in`;
+
+    console.log('HERE');
+  });
+
   const onSubmitButtonClick = () => {
+    console.log({ password });
     if (user.loggedIn) {
       return;
     }
-    setUser({ userId: 1, loggedIn: true });
 
-    if (location.state?.from) {
-      navigate(location.state.from);
-    }
+    const loginData = {
+      password: password,
+      login: login,
+    };
+    mutate(loginData);
   };
 
   return (
@@ -50,8 +68,18 @@ const LogIn = () => {
           Please log in.
         </FormText>
         <InputsWrapper>
-          <FormEmail type="text" name="email" placeholder="Email" />
-          <FormPassword type="password" name="password" placeholder="Password" />
+          <FormEmail
+            type="text"
+            name="email"
+            placeholder="Email"
+            onChange={(e) => setLogin(e.target.value)}
+          />
+          <FormPassword
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
           <FormButtons>
             <FormSubmit onClick={onSubmitButtonClick}>Submit!</FormSubmit>
