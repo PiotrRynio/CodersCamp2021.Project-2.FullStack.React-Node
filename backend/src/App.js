ddccfb114fbac3615caba3d408589bfa816f09bb
Piotr Rynio <pwrynio@gmail.com>
2022-03-20 21:11:03 +0100

    fix(swagger): fix after marge

--- backend/src/App.js
+++ backend/src/App.js
@@ -12,12 +12,6 @@ import { InMemoryUsersRepository } from './modules/UsersRegistration/repository/
 import { announcementsModule } from './modules/Announcements/AnnouncementsModule.js';
 import { boardsModule } from './modules/Boards/boardsModule.js';
 import { commentModule } from './modules/AddComment/CommentModule.js';
-import { UsersRegistrationController } from './modules/UsersRegistration/controller/UsersRegistration.controller.js';
-import { UserRegistrationService } from './modules/UsersRegistration/service/UserRegistration.service.js';
-import { MongoUsersRegistrationRepository } from './modules/UsersRegistration/repository/mongo/MongoUsersRegistration.repository.js';
-import { InMemoryUsersRegistrationRepository } from './modules/UsersRegistration/repository/inMemory/InMemoryUsersRegistration.repository.js';
-import { CommentModule } from './modules/AddComment/CommentModule.js';
-import { boardsModule } from './modules/Boards/boardsModule.js';
 
 dotenv.config();
 
@@ -42,9 +36,6 @@ export const app = async () => {
   restApiServer.use('/rest-api', commentModule(repositoryType));
   restApiServer.use('/rest-api', announcementController.router);
   restApiServer.use('/rest-api', boardsController.router);
-
-  restApiServer.use('/rest-api', CommentModule(repositoryType));
-  restApiServer.use('/rest-api', boardsModule(repositoryType));
 
   restApiServer.use('/rest-api-documentation', swaggerUi.serve);
   restApiServer.use('/rest-api-documentation', swaggerUi.setup(swaggerDocumentation));
