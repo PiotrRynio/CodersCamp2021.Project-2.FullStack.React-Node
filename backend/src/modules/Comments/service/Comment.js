570006b1e3e7cc0a5abef153c8991983e35c7698
Patryk Święcicki <patswiecicki@gmail.com>
2022-03-23 01:41:36 +0100

    fix(comments): Fix after merge

--- backend/src/modules/Comments/service/Comment.js
+++ backend/src/modules/Comments/service/Comment.js
@@ -1,7 +1,13 @@
 import { v4 as uuidv4 } from 'uuid';
+import { validateComment } from './CommentValidationSchema.js';
 
 export class Comment {
   constructor(props) {
+    const { error } = validateComment(props);
+    if (error) {
+      throw new Error(error.details[0].message);
+    }
+
     const { userId, content, date, id, authorFirstName, authorLastName, authorAvatarUrl } = props;
     this.id = id || uuidv4();
     this.userId = userId || 'Testid';
