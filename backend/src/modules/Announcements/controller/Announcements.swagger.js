d2e8f732f1a32d22ff6f8cdf6c7a1055df3941c2
Patryk Święcicki <patswiecicki@gmail.com>
2022-03-25 11:12:47 +0100

    feat(comments): Announcements swagger documentation

--- backend/src/modules/Announcements/controller/Announcements.swagger.js
+++ backend/src/modules/Announcements/controller/Announcements.swagger.js
@@ -10,7 +10,7 @@ export const announcementsSwagger = {
           in: 'path',
           description: 'id of the board',
           type: 'string',
-          example: '1234-5678-9012-3456',
+          example: '62379b22771651aecc5ed07c',
         },
       ],
       requestBody: {
@@ -53,19 +53,295 @@ export const announcementsSwagger = {
               schema: {
                 type: 'object',
                 example: {
-                  announcements: 'announcements - uzupełnić (?)',
+                  announcements: [
+                    {
+                      id: '62379b22771651aecc5ed07c',
+                      title: 'Hello World',
+                      boardName: 'Hello Board',
+                      iconType: 'default',
+                      content:
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                      commentsIds: [],
+                      date: '2022-03-20T21:22:42.039Z',
+                    },
+                    {
+                      id: '62379b3c771651aecc5ed085',
+                      title: 'Second',
+                      boardName: 'Wrocław',
+                      iconType: 'default',
+                      content: 'Jakiś inny content na minimum 20 znaków',
+                      commentsIds: ['623a6281ac0cb9c066772949'],
+                      date: '2022-03-20T21:23:08.090Z',
+                    },
+                  ],
                 },
               },
             },
           },
         },
-        405: {
+        400: {
           description: 'Validation exception',
           content: {
             'application/json': {
               schema: {
                 type: 'object',
-                example: { message: 'data and salt arguments required - uzupełnić (?)' },
+                example: { message: 'Content is required' },
+              },
+            },
+          },
+        },
+      },
+    },
+    get: {
+      tags: ['Announcements'],
+      description: 'get board announcements',
+      summary: 'Gets one board announcements',
+      parameters: [
+        {
+          name: 'id',
+          in: 'path',
+          description: 'id of the board',
+          type: 'string',
+          example: '62379b22771651aecc5ed07c',
+        },
+      ],
+      responses: {
+        200: {
+          description: 'OK',
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                example: {
+                  announcements: [
+                    {
+                      id: '62379b22771651aecc5ed07c',
+                      title: 'Hello World',
+                      boardName: 'Hello Board',
+                      iconType: 'default',
+                      content:
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                      commentsIds: [],
+                      date: '2022-03-20T21:22:42.039Z',
+                    },
+                    {
+                      id: '62379b3c771651aecc5ed085',
+                      title: 'Second',
+                      boardName: 'Wrocław',
+                      iconType: 'default',
+                      content: 'Jakiś inny content na minimum 20 znaków',
+                      commentsIds: ['623a6281ac0cb9c066772949'],
+                      date: '2022-03-20T21:23:08.090Z',
+                    },
+                  ],
+                },
+              },
+            },
+          },
+        },
+        404: {
+          description: 'Validation exception',
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                example: { message: 'Board not found' },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  '/rest-api/announcements/{id}': {
+    get: {
+      tags: ['Announcements'],
+      description: 'get announcement',
+      summary: 'Gets one announcement data by id',
+      parameters: [
+        {
+          name: 'id',
+          in: 'path',
+          description: 'id of the announcement',
+          type: 'string',
+          example: '62379b22771651aecc5ed07c',
+        },
+      ],
+      responses: {
+        200: {
+          description: 'OK',
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                example: {
+                  announcement: {
+                    id: '62379b22771651aecc5ed07c',
+                    title: 'Hello World',
+                    boardName: 'Hello Board',
+                    iconType: 'default',
+                    content:
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                    commentsIds: [],
+                    date: '2022-03-20T21:22:42.039Z',
+                  },
+                },
+              },
+            },
+          },
+        },
+        400: {
+          description: 'Exceptions',
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                example: { message: 'Error message' },
+              },
+            },
+          },
+        },
+        404: {
+          description: 'Not found exception',
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                example: { message: 'Announcement not found' },
+              },
+            },
+          },
+        },
+      },
+    },
+    patch: {
+      tags: ['Announcements'],
+      description: 'update announcement',
+      summary: 'Change announcement body',
+      parameters: [
+        {
+          name: 'id',
+          in: 'path',
+          description: 'id of the announcement',
+          type: 'string',
+          example: '62379b22771651aecc5ed07c',
+        },
+      ],
+      requestBody: {
+        content: {
+          'application/json': {
+            schema: {
+              type: 'object',
+              properties: {
+                title: {
+                  type: 'string',
+                  description: 'New announcement title',
+                  example: 'Hello World',
+                },
+                content: {
+                  type: 'string',
+                  description: 'New announcement content',
+                  example: 'New text fot announcement without lorem ipsum.',
+                },
+                iconType: {
+                  type: 'string',
+                  description: 'New Announcement type of icon',
+                  example: 'electricity',
+                },
+              },
+            },
+          },
+        },
+      },
+      responses: {
+        200: {
+          description: 'OK',
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                example: {
+                  announcement: {
+                    id: '62379b22771651aecc5ed07c',
+                    title: 'New announcement title',
+                    boardName: 'Hello Board',
+                    iconType: 'electricity',
+                    content: 'New text fot announcement without lorem ipsum.',
+                    commentsIds: [],
+                    date: '2022-03-20T21:22:42.039Z',
+                  },
+                },
+              },
+            },
+          },
+        },
+        400: {
+          description: 'Exceptions',
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                example: { message: 'Error message' },
+              },
+            },
+          },
+        },
+        404: {
+          description: 'Not found exception',
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                example: { message: 'Announcement not found.' },
+              },
+            },
+          },
+        },
+      },
+    },
+    delete: {
+      tags: ['Announcements'],
+      description: 'delete announcement',
+      summary: 'Remove announcement and announcement id from board announcements list',
+      parameters: [
+        {
+          name: 'id',
+          in: 'path',
+          description: 'id of the announcement',
+          type: 'string',
+          example: '62379b22771651aecc5ed07c',
+        },
+      ],
+      responses: {
+        200: {
+          description: 'OK',
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                example: {
+                  announcement: {
+                    id: '62379b22771651aecc5ed07c',
+                    title: 'Hello World',
+                    boardName: 'Hello Board',
+                    iconType: 'default',
+                    content:
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                    commentsIds: [],
+                    date: '2022-03-20T21:22:42.039Z',
+                  },
+                },
+              },
+            },
+          },
+        },
+        400: {
+          description: 'Exceptions',
+          content: {
+            'application/json': {
+              schema: {
+                type: 'object',
+                example: { message: 'Error message' },
               },
             },
           },
